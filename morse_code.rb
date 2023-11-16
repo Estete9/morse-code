fed8969823c9c9bc0034a9017a8f28d6f94f2ce1
Esteban <cepalacios@outlook.com>
2023-11-16 17:49:27 -0500

    Add posible solutions to translate and to return the flattened new translation

--- morse_code.rb
+++ morse_code.rb
@@ -4,13 +4,21 @@
 
 # seperarte `words` in `letters` using `split` and store it
 
-#loop through the new array of arrays we got from the previos 2 steps
-  # loop through the inside array (using map! to update in place)
-    # use the current element as key in the alphabet hash
-    # modify in place the current element in the array with the value matched with the key
+# translate each morse_letter into a letter
+# ----------possible solution--------------
+# nested_array.map! do |arr|
+#   arr.map! { |morse_letter| decode_table[morse_letter] }
+# end
+# -----------------------------------------
+  #loop through the new array of arrays we got from the previos 2 steps
+    # loop through the inside array (using map! to update in place)
+      # use the current element as key in the alphabet hash
+      # modify in place the current element in the array with the value matched with the key
 
 # flatten the array
-# ----------possible solution to flatten the array: `nested_array.map!(&:join).join(" ")`-----------
+# ----------possible solution--------------
+# nested_array.map!(&:join).join(" ")
+# -----------------------------------------
   #loop through the array of arrays (using map! to update in place)
     #join without seperator
   # Joing the array of arrays (now is only a normal array) with join and a space as seperator
