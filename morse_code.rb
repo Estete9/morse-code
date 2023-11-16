d999860a192bea4fb932a5cf83578416d0e290b0
Esteban <cepalacios@outlook.com>
2023-11-16 15:17:10 -0500

    Describe steps needed to Decode morse code

---
+++ morse_code.rb
@@ -0,0 +1,12 @@
+# create a hash with the Morse code for each letter as keys and the alphabet as values
+# sepeare sentence in words
+# seperarte words in letters
+#loop through the new array of arrays
+  # loop through the inside array
+    # use the current element as key in the alphabet hash
+    # modify in place the current element in the array with the value matched with the key
+# flatten the array
+  #loop through the array of arrays
+    #join without seperator
+  # Joing the array of arrays (now is only a normal array) with join and a space as seperator
+  # return the modified variable that now is a string
