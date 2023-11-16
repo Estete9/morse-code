9a5192e233a48ae86162e8fcc59becb274e75a14
Esteban <cepalacios@outlook.com>
2023-11-16 17:02:32 -0500

    update thought process to especify how to iterate through the array

--- morse_code.rb
+++ morse_code.rb
@@ -2,11 +2,11 @@
 # sepeare sentence in words
 # seperarte words in letters
 #loop through the new array of arrays
-  # loop through the inside array
+  # loop through the inside array (using map! to update in place)
     # use the current element as key in the alphabet hash
     # modify in place the current element in the array with the value matched with the key
 # flatten the array
-  #loop through the array of arrays
+  #loop through the array of arrays (using map! to update in place)
     #join without seperator
   # Joing the array of arrays (now is only a normal array) with join and a space as seperator
   # return the modified variable that now is a string
